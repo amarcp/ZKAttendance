fa2e6ef51f6fabf94638f12d6e0d1b624e9d615d
Amar <amar@vinam.in>
2019-06-13 17:54:49 +0530

    fixed sample code

--- sample.php
+++ sample.php
@@ -1,1 +1,15 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use \ZKAttendance\ZKLibrary;
+
+$zk = new ZKLibrary('172.17.254.4',4370);
+$zk->connect();
+foreach ( $zk->getAttendance() as $att){
+	print_r($att);
+
+}
+
+foreach ( $zk->getUser() as $user){
+	print_r($user);
+}
+#print_r($zk);
